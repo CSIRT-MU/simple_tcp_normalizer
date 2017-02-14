9939f8e5d35cd43cd7ace67c443c61b693917ff6
xdanos <danos@mail.muni.cz>
2017-02-14 11:13:50 +0100

    relative path fix

--- bin/tcp_normalizer.sh
+++ bin/tcp_normalizer.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-java -XX:+UseConcMarkSweepGC -Xms1024m -Xmx1024m -jar target/simple_tcp_normalizer-0.1-SNAPSHOT-jar-with-dependencies.jar "$@"
+DIR="${BASH_SOURCE%/*}"
+java -XX:+UseConcMarkSweepGC -Xms1024m -Xmx1024m -jar ${DIR}/../target/simple_tcp_normalizer-0.1-SNAPSHOT-jar-with-dependencies.jar "$@"
